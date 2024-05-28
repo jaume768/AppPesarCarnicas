40e74755b42b5ad7d44107f1236ee5fa99a91733
jaume768 <jaumefernandezsunyer10@gmail.com>
2024-05-28 08:10:48 +0200

    side buttons refactorizados

--- lib/ui/widgets/side_buttons.dart
+++ lib/ui/widgets/side_buttons.dart
@@ -18,152 +18,172 @@ class SideButtons extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: _buildButton(context, 'REVISAR COMPLET', Colors.lightBlue, () {}),
-              ),
-              Expanded(
-                child: _buildButton(context, 'CANVIAR CONFIGURACIÓ', Colors.yellow.shade300, () {
-                  Navigator.pop(context);
-                }),
-              ),
-            ],
-          ),
-          SizedBox(height: 50),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Expanded(
-                child: _buildButton(context, 'FILTRAR CLIENT', Colors.green.shade300, onFilterClient),
-              ),
-              SizedBox(width: 10),
-              Expanded(
-                child: _buildButton(context, 'MARCAR PENDENT', Colors.grey.shade300, () {
-                  final currentState = BlocProvider.of<ProductBloc>(context).state;
-                  if (currentState is ProductLoaded) {
-                    BlocProvider.of<ProductBloc>(context).add(MarkAsPending(currentState.selectedArticle));
-                  }
-                }),
-              ),
-              Expanded(
-                child: BlocBuilder<ProductBloc, ProductState>(
-                  builder: (context, state) {
-                    final isEnabled = state is ProductLoaded && state.selectedArticle != -1;
-                    final buttonText = (state is ProductLoaded && state.isSpecial) ? 'ETIQUETA BLOC' : 'PES PRODUCTE';
-                    return _buildButton(
-                      context,
-                      buttonText,
-                      isEnabled ? Colors.orange : Colors.grey.shade300,
-                      isEnabled ? () {
-                        print('Procesar artículo seleccionado: ${state.selectedArticle}');
-                      } : null,
-                    );
-                  },
-                ),
-              ),
-              SizedBox(width: 10),
-            ],
-          ),
-          SizedBox(height: 50),
+          _buildTopButtons(context),
+          const SizedBox(height: 50),
+          _buildMiddleButtons(context),
+          const SizedBox(height: 50),
           Center(
             child: _buildButton(context, 'ENVIAR FINAL', Colors.grey.shade300, () {}),
           ),
-          Spacer(),
-          Column(
-            children: [
-              BlocBuilder<ProductBloc, ProductState>(
-                builder: (context, state) {
-                  if (state is ProductLoaded && state.isMandatoryLot) {
-                    return Container(
-                      margin: EdgeInsets.only(bottom: 20,left: 165),
-                      child: Row(
-                        children: [
-                          Text('LOT:', style: TextStyle(color: Colors.black, fontSize: 24)),
-                          SizedBox(width: 10),
-                          ElevatedButton(
-                            onPressed: () {
-                              // Acción para el botón "S/N"
-                            },
-                            child: Text('S/N', style: TextStyle(color: Colors.black, fontSize: 24)),
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.pink,
-                              minimumSize: Size(100, 60),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(0),
-                              ),
-                              side: BorderSide(color: Colors.black, width: 1),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                  return SizedBox.shrink();
-                },
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 20),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 1,
-                  ),
-                ),
-                child: Container(
-                  color: Colors.green.shade200,
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    '0,00',
-                    style: TextStyle(color: Colors.black, fontSize: 42),
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(right: 50),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Multi Pes', style: TextStyle(color: Colors.black, fontSize: 24)),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.pink,
-                        minimumSize: Size(100, 100),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(0),
-                        ),
-                        side: BorderSide(color: Colors.black, width: 1),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(right: 50),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Aceptar Pesada,', style: TextStyle(color: Colors.black, fontSize: 24)),
-                          Text('gravar, imprimir', style: TextStyle(color: Colors.black, fontSize: 24)),
-                        ],
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blueAccent,
-                        minimumSize: Size(200, 100),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(0),
-                        ),
-                        side: BorderSide(color: Colors.black, width: 1),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+          const Spacer(),
+          _buildBottomSection(context),
         ],
+      ),
+    );
+  }
+
+  Widget _buildTopButtons(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: _buildButton(context, 'REVISAR COMPLET', Colors.lightBlue, () {}),
+        ),
+        Expanded(
+          child: _buildButton(context, 'CANVIAR CONFIGURACIÓ', Colors.yellow.shade300, () {
+            Navigator.pop(context);
+          }),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMiddleButtons(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          child: _buildButton(context, 'FILTRAR CLIENT', Colors.green.shade300, onFilterClient),
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+          child: _buildMarkAsPendingButton(context),
+        ),
+        Expanded(
+          child: _buildBlocButton(context),
+        ),
+        const SizedBox(width: 10),
+      ],
+    );
+  }
+
+  Widget _buildMarkAsPendingButton(BuildContext context) {
+    return _buildButton(context, 'MARCAR PENDENT', Colors.grey.shade300, () {
+      final currentState = BlocProvider.of<ProductBloc>(context).state;
+      if (currentState is ProductLoaded) {
+        BlocProvider.of<ProductBloc>(context).add(MarkAsPending(currentState.selectedArticle));
+      }
+    });
+  }
+
+  Widget _buildBlocButton(BuildContext context) {
+    return BlocBuilder<ProductBloc, ProductState>(
+      builder: (context, state) {
+        final isEnabled = state is ProductLoaded && state.selectedArticle != -1;
+        final buttonText = (state is ProductLoaded && state.isSpecial) ? 'ETIQUETA BLOC' : 'PES PRODUCTE';
+        return _buildButton(
+          context,
+          buttonText,
+          isEnabled ? Colors.orange : Colors.grey.shade300,
+          isEnabled ? () {
+            print('Procesar artículo seleccionado: ${state.selectedArticle}');
+          } : null,
+        );
+      },
+    );
+  }
+
+  Widget _buildBottomSection(BuildContext context) {
+    return Column(
+      children: [
+        _buildMandatoryLotSection(context),
+        _buildAmountDisplay(),
+        _buildBottomButtons(),
+      ],
+    );
+  }
+
+  Widget _buildMandatoryLotSection(BuildContext context) {
+    return BlocBuilder<ProductBloc, ProductState>(
+      builder: (context, state) {
+        if (state is ProductLoaded && state.isMandatoryLot) {
+          return Container(
+            margin: EdgeInsets.only(bottom: 20, left: 165),
+            child: Row(
+              children: [
+                Text('LOT:', style: TextStyle(color: Colors.black, fontSize: 24)),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción para el botón "S/N"
+                  },
+                  child: Text('S/N', style: TextStyle(color: Colors.black, fontSize: 24)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.pink,
+                    minimumSize: Size(100, 60),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                    side: BorderSide(color: Colors.black, width: 1),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+        return SizedBox.shrink();
+      },
+    );
+  }
+
+  Widget _buildAmountDisplay() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black,
+          width: 1,
+        ),
+      ),
+      child: Container(
+        color: Colors.green.shade200,
+        padding: EdgeInsets.all(8.0),
+        child: Text(
+          '0,00',
+          style: TextStyle(color: Colors.black, fontSize: 42),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBottomButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        _buildCustomButton('Multi Pes', Colors.pink, 100, 100, () {}),
+        _buildCustomButton('Aceptar Pesada,\ngravar, imprimir', Colors.blueAccent, 200, 100, () {}),
+      ],
+    );
+  }
+
+  Widget _buildCustomButton(String text, Color color, double width, double height, VoidCallback onPressed) {
+    return Container(
+      margin: EdgeInsets.only(right: 50),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.black, fontSize: 24),
+          textAlign: TextAlign.center,
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          minimumSize: Size(width, height),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(0),
+          ),
+          side: BorderSide(color: Colors.black, width: 1),
+        ),
       ),
     );
   }
