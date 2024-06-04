c50a9a1a3b8a7ddbd15258f8bbeff10d28fab05b
jaume768 <jaumefernandezsunyer10@gmail.com>
2024-06-04 11:11:09 +0200

    Els botons es generen a partir del json

--- lib/ui/widgets/category_column.dart
+++ lib/ui/widgets/category_column.dart
@@ -1,11 +1,33 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:flutter/services.dart' show rootBundle;
 import '../../structure/bloc/carniceria/carniceria_bloc.dart';
 import '../../structure/bloc/carniceria/carniceria_event.dart';
 import '../../structure/bloc/carniceria/carniceria_state.dart';
 
-class CategoryColumn extends StatelessWidget {
+class CategoryColumn extends StatefulWidget {
+  @override
+  _CategoryColumnState createState() => _CategoryColumnState();
+}
+
+class _CategoryColumnState extends State<CategoryColumn> {
+  List<dynamic> buttons = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadButtons();
+  }
+
+  Future<void> _loadButtons() async {
+    final String response = await rootBundle.loadString('assets/data/botones_categorias.json');
+    final data = await json.decode(response);
+    setState(() {
+      buttons = data['buttons'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -13,24 +35,21 @@ class CategoryColumn extends StatelessWidget {
         builder: (context, state) {
           return Column(
             children: [
+              Container(
+                margin: EdgeInsets.only(top: 60), // Margen superior para el botón de carnicería
+                child: _buildSwitch(context, 'CARNICERIA    ', state.isButchery),
+              ),
               Expanded(
                 child: GridView.count(
                   crossAxisCount: 2,
-                  childAspectRatio: 1,
+                  childAspectRatio: 1.2,
                   mainAxisSpacing: 10,
-                  padding: EdgeInsets.only(top: 70, left: 30),
+                  padding: EdgeInsets.only(top: 20, left: 30, right: 30),
                   crossAxisSpacing: 10,
-                  children: [
-                    _buildButton(context, 'Avicola', 'POULTRY', state.selectedProductType),
-                    _buildButton(context, 'Porc', 'PIG', state.selectedProductType),
-                    _buildButton(context, 'Me / Cordero', 'LAMB', state.selectedProductType),
-                    _buildButton(context, 'Tallat / Desfet', 'CHOPPED', state.selectedProductType),
-                  ],
+                  children: buttons.map<Widget>((button) {
+                    return _buildButton(context, button['text'], button['productType'], state.selectedProductType);
+                  }).toList(),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 80.0),
-                child: _buildSwitch(context, 'CARNICERIA    ', state.isButchery),
               ),
             ],
           );
@@ -43,19 +62,26 @@ class CategoryColumn extends StatelessWidget {
     bool isSelected = selectedProductType == productType;
     Color color = isSelected ? Colors.green : Colors.grey.shade300;
 
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        foregroundColor: Colors.black,
-        backgroundColor: color,
-        minimumSize: Size(100, 100),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(0),
+    return Container(
+      margin: EdgeInsets.only(top: 10), // Margen superior
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.black,
+          backgroundColor: color,
+          minimumSize: Size(100, 50), // Ajustar el tamaño mínimo del botón
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8), // Bordes redondeados
+          ),
+        ),
+        onPressed: () {
+          context.read<CarniceriaBloc>().add(FetchSummaries(productType));
+        },
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20), // Ajustar el tamaño del texto
         ),
       ),
-      onPressed: () {
-        context.read<CarniceriaBloc>().add(FetchSummaries(productType));
-      },
-      child: Text(text, textAlign: TextAlign.center, style: TextStyle(fontSize: 26),),
     );
   }
 
@@ -63,7 +89,10 @@ class CategoryColumn extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(text, style: TextStyle(fontSize: 26),),
+        Text(
+          text,
+          style: TextStyle(fontSize: 26),
+        ),
         Transform.scale(
           scale: 1.4,
           child: Switch(
